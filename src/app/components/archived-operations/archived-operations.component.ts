859193d8c33db74a08cae1b485441c84a6c9abec
safderun <burakberkkeskin@gmail.com>
2021-04-18 22:49:42 +0300

    Kar zarar hesaplama

--- src/app/components/archived-operations/archived-operations.component.ts
+++ src/app/components/archived-operations/archived-operations.component.ts
@@ -12,4 +12,26 @@ export class ArchivedOperationsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  totalCostCalculate(amount:number, cost:number):number{
+    return amount*cost
+  }
+
+  totalValueCalculate(amount:number, price:number):number{
+    return amount*price
+  }
+
+  profitLoss(amount:number, cost:number, price:number):number{
+    let pnl:number = (amount*price)-(amount*cost)
+    let pnls:string = pnl.toFixed(2)
+    pnl = +pnls
+    return pnl
+  }
+
+  profitLossPercent(cost:number, price:number):number{
+    let pnl:number = ((price-cost)/price)*100
+    let pnls:string = pnl.toFixed(2)
+    pnl = +pnls
+    return pnl
+  }
+
 }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -28,7 +28,7 @@ export class DashboardComponent implements OnInit {
   }
 
   profitLossPercent(cost:number, price:number):number{
-    let pnl:number = (price/cost)*100
+    let pnl:number = ((price-cost)/price)*100
     let pnls:string = pnl.toFixed(2)
     pnl = +pnls
     return pnl
