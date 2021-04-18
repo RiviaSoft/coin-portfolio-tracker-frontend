1b51fca51b474cc2093bcdd5d553c2c8b24ffd3c
safderun <burakberkkeskin@gmail.com>
2021-04-18 22:42:33 +0300

    Anlık kar zarar fonksiyonları

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  toplamMaliyet:number
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  totalCostCalculate(amount:number, cost:number):number{
+    return amount*cost
+  }
+
+  totalValueCalculate(amount:number, price:number):number{
+    return amount*price
+  }
+
+  profitLoss(amount:number, cost:number, price:number):number{
+    let pnl:number = (amount*price)-(amount*cost)
+    let pnls:string = pnl.toFixed(2)
+    pnl = +pnls
+    return pnl
+  }
+
+  profitLossPercent(cost:number, price:number):number{
+    let pnl:number = (price/cost)*100
+    let pnls:string = pnl.toFixed(2)
+    pnl = +pnls
+    return pnl
+  }
+
+
 
 }
