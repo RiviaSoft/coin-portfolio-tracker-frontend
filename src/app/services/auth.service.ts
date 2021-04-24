7bbf961cd8d3e5be8f78eb0559e24d54a6c9619b
safderun <burakberkkeskin@gmail.com>
2021-04-24 22:37:35 +0300

    register servis yazıldı

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { ResultModel } from '../models/resultModel';
 import { TokenModel } from '../models/tokenModel';
 import { UserModel } from '../models/userModel';
 
@@ -13,7 +14,8 @@ export class AuthService {
   constructor(private httpClient:HttpClient) { }
 
   login(userModel:UserModel):Observable<TokenModel>{
-      return this.httpClient.post<TokenModel>(this.apiUrl+"login", userModel)
+    let newPath = this.apiUrl+"login"
+    return this.httpClient.post<TokenModel>(newPath, userModel)
   }
 
   isAuthenticated():boolean{
@@ -24,6 +26,9 @@ export class AuthService {
     }
   }
 
-
+  register(userModel:UserModel):Observable<ResultModel>{
+    let newPath = this.apiUrl + "register"
+    return this.httpClient.post<ResultModel>(newPath, userModel)
+  }
 
 }
