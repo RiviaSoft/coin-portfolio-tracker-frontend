80b7706bc4bbbeb611c616d7292ae1c519276623
safderun <burakberkkeskin@gmail.com>
2021-04-30 22:19:14 +0300

    coinPairs local dosyası güncellendi

--- src/app/services/binance-service.service.ts
+++ src/app/services/binance-service.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { coinPairs } from '../models/coinPairs';
-import coins from 'src/assets/coins.json';
+// import { coinPairs } from '../models/coinPairs';
+import coinPairs from 'src/assets/coinPairs.json';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,26 +14,28 @@ export class BinanceServiceService {
     private httpclient:HttpClient
   ) {}
 
-  coinPairs:string[]
+  coinPairsList:string[]=[]
   getCoinPairs(){
-    this.coinPairs = coins
-    return this.coinPairs
+    coinPairs.forEach(element => {
+      this.coinPairsList.push(element.symbol)
+    });
+    return this.coinPairsList
   }
 
-  async getCoins(){
+  // async getCoins(){
   
-    let symbols:coinPairs []=[]
-    const api = "https://api3.binance.com/api/v3/exchangeInfo"
-    let r:any = await fetch(api)
-    r = await r.json()
-    r.symbols.forEach((element:any, i:number) => {
-      symbols.push({
-        item_id:i,
-        item_text:element.symbol
-      })
-    })
-    return symbols
-  }
+  //   let symbols:coinPairs []=[]
+  //   const api = "https://api3.binance.com/api/v3/exchangeInfo"
+  //   let r:any = await fetch(api)
+  //   r = await r.json()
+  //   r.symbols.forEach((element:any, i:number) => {
+  //     symbols.push({
+  //       item_id:i,
+  //       item_text:element.symbol
+  //     })
+  //   })
+  //   return symbols
+  // }
   
   
 
