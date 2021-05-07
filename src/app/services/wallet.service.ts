23481d0160fb79d91a28253e765d09f424429aa3
safderun <burakberkkeskin@gmail.com>
2021-05-07 11:44:21 +0300

    cüzdan ekleme, silme tamamlandı

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { WalletModel } from 'src/app/models/walletModel';
 import { Observable } from 'rxjs';
+import { ResultModel } from '../models/resultModel';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,37 @@ import { Observable } from 'rxjs';
 export class WalletService {
 
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(
+    private httpClient:HttpClient,
+    ) { }
 
   apiUrl:string= environment.apiUrl + "/api/"
 
   getWallets():Observable<WalletModel[]>{
     let newPath = this.apiUrl+"wallets/getall"
     return this.httpClient.get<WalletModel[]>(newPath)
+  }
+
+  addWallet(wallet:WalletModel):Observable<ResultModel>{
+    let newPath = this.apiUrl+"wallets/add"
+    return this.httpClient.post<ResultModel>(newPath, wallet)
+  }
+
+  deleteWallet(wallet:WalletModel):Observable<ResultModel>{
+    let newPath = this.apiUrl+"wallets/delete"
+    return this.httpClient.post<ResultModel>(newPath, wallet)
+  }
+
+  getWalletOperations(){
+
+  }
+
+  addWalletOperation(){
+
+  }
+
+  deleteWalletOperation(){
+    
   }
 
 }
